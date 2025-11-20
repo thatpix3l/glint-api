6ad3a92760861b66d27ad85a89dcd76cb9f9a0d8
thatpix3l <git@thatpix3l.com>
2025-11-20 12:35:35 -0500

    fix: move top-level `await` to dedicated function

--- src/main.ts
+++ src/main.ts
@@ -8,61 +8,65 @@ import cookieParser from "cookie-parser";
 import { Authenticate } from "./middleware/authentication";
 import path from "path";
 
-// Initiate connection to MongoDB
-console.log("Connecting to MongoDB...");
-await GS.mongo.client.connect();
+async function main() {
 
-// Default express app
-const app = exp.default();
+    // Initiate connection to MongoDB
+    console.log("Connecting to MongoDB...");
+    await GS.mongo.client.connect();
 
-// Middleware
-app.use(cookieParser());
-app.use(Authenticate);
+    // Default express app
+    const app = exp.default();
 
-// API Routes
-app.use(exp.json());
-RegisterRoutes(app);
+    // Middleware
+    app.use(cookieParser());
+    app.use(Authenticate);
 
-// Swagger UI Docs
-app.use("/api/docs", sw_ui.serve);
-app.get("/api/docs", sw_ui.setup(spec));
+    // API Routes
+    app.use(exp.json());
+    RegisterRoutes(app);
 
-// Frontend
-app.use(exp.static('public'));
+    // Swagger UI Docs
+    app.use("/api/docs", sw_ui.serve);
+    app.get("/api/docs", sw_ui.setup(spec));
 
-// Catch-all for WebUI routes
-app.get("*", (_, res) => {
-    res.sendFile(path.resolve("./public", "index.html"));
-});
+    // Frontend
+    app.use(exp.static('public'));
 
-// Error handling middleware
-app.use(function errorHandler(
-    err: unknown,
-    req: exp.Request,
-    res: exp.Response,
-    next: exp.NextFunction
-): exp.Response | void {
-    if (err instanceof ValidateError) {
-        console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
-        return res.status(422).json({
-            message: "Validation Failed",
-            details: err?.fields,
-        });
-    }
-    if (err instanceof Error) {
-        console.warn(err.message);
-        return res.status(500).json({
-            message: "Internal Server Error"
-        });
-    }
+    // Catch-all for WebUI routes
+    app.get("*", (_, res) => {
+        res.sendFile(path.resolve("./public", "index.html"));
+    });
 
-    next();
-});
+    // Error handling middleware
+    app.use(function errorHandler(
+        err: unknown,
+        req: exp.Request,
+        res: exp.Response,
+        next: exp.NextFunction
+    ): exp.Response | void {
+        if (err instanceof ValidateError) {
+            console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+            return res.status(422).json({
+                message: "Validation Failed",
+                details: err?.fields,
+            });
+        }
+        if (err instanceof Error) {
+            console.warn(err.message);
+            return res.status(500).json({
+                message: "Internal Server Error"
+            });
+        }
+
+        next();
+    });
 
 
-// Blocking listen.
-app.listen(GS.api.port, GS.api.address, () => {
-    console.log(`Listening on ${GS.api.address}:${GS.api.port}`);
-})
+    // Blocking listen.
+    app.listen(GS.api.port, GS.api.address, () => {
+        console.log(`Listening on ${GS.api.address}:${GS.api.port}`);
+    })
 
-export default app;
+}
+
+main()
